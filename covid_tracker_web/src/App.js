e0cf57e6449bee3476e75845e611a30d50ddfc71
MatthewG-Tech <matthewbguerra@yahoo.com>
2020-06-09 07:27:26 -0700

    Remove Default React

--- covid_tracker_web/src/App.js
+++ covid_tracker_web/src/App.js
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jss</code> and save to reload at start.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          App
+        </h1>
       </header>
     </div>
   );
