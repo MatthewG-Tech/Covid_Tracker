b4b6cd043af8475177e68f3cf9aca070dc14acdf
MatthewG-Tech <matthewbguerra@yahoo.com>
2020-06-18 13:32:43 -0700

    Docker backend built. Serivce Account file redacted.

--- covid_tracker_docker_backend/index.js
+++ covid_tracker_docker_backend/index.js
@@ -2,24 +2,75 @@
 const {BigQuery} = require('@google-cloud/bigquery');
 const admin = require('firebase-admin');
 
-//Pathway to service key
-let serviceAccount = require('../secure_files/covid-tracker-101-fd0e87e6a712.json');
+// Imports scheduler
+var schedule = require('node-schedule');
 
-//Initilize the database
+// Pathway to service key
+let serviceAccount = require('./covid-tracker-101-fd0e87e6a712.json');
+
+// Initilize the database
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
   
 let db = admin.firestore();
 
-// Creates a client
-const bigquery = new BigQuery();
+// Set global date
+var date = new Date();
+var month = date.getMonth() + 1
+var day = date.getDate() - 1
 
-const query = 'SELECT country_region, SUM(confirmed) FROM `bigquery-public-data.covid19_jhu_csse.summary` group by country_region LIMIT 1000';
+// Add deaths to the data base
+function addDeaths(){
+    try{
+        // Get the Query
+        const bigquery = new BigQuery();
 
-bigquery.query(query, function(err, rows) {
-if (!err) {
-    // rows is an array of results.
-    console.log(rows);
+        const query = 'SELECT ' + 'SUM(_' + month + '_' + day + '_20), ' + ' country_region FROM `bigquery-public-data.covid19_jhu_csse.deaths` group by country_region order by 1 DESC';
+
+        bigquery.query(query, function(err, rows) {
+            if (!err) {
+                // Go through and add data
+                for(let i = 0; i < rows.length; i++){
+                    let data = {}
+                    data['_' + month + '_' + day + '_20'] = rows[i]["f0_"]
+                    db.collection('deaths').doc(rows[i]['country_region']).update(data)
+                }
+            }
+            console.log(err)
+        });
+        console.log("Add deaths")
+    }catch(e){
+        console.error(e);
+    }
 }
+
+// Add cases
+function addCases(){
+    try{
+        // Get the Query
+        const bigquery = new BigQuery();
+
+        const query = 'SELECT ' + 'SUM(_' + month + '_' + day + '_20), ' + ' country_region FROM `bigquery-public-data.covid19_jhu_csse.confirmed_cases` group by country_region order by 1 DESC';
+
+        bigquery.query(query, function(err, rows) {
+            if (!err) {
+                // Go through and add data
+                for(let i = 0; i < rows.length; i++){
+                    let data = {}
+                    data['_' + month + '_' + day + '_20'] = rows[i]["f0_"]
+                    db.collection('cases').doc(rows[i]['country_region']).update(data)
+                }
+            }
+            console.log(err)
+        });
+        console.log("Add cases")
+    }catch(e){
+        console.error(e);
+    }
+}
+var deathsScheduler = schedule.scheduleJob('0 0 1 * * *', function(){
+    addDeaths();
+    addCases();
+    console.log("Seceduler ran");
 });
