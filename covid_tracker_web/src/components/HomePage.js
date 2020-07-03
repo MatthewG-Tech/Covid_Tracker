c1130b4c9785e8b175f1e0991ca4b7ae8dd9e766
MatthewG-Tech <matthewbguerra@yahoo.com>
2020-07-03 07:41:59 -0700

    Add License

--- covid_tracker_web/src/components/HomePage.js
+++ covid_tracker_web/src/components/HomePage.js
@@ -5,7 +5,7 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className="contentBody">
-                <p>To get started, select a state from the sidebar. Multiple states can be added to compare data.</p>
+                <p>To get started, select a state from the sidebar. Multiple states can be selected from the sidebar to compare data.</p>
                 <br />
                 <p>This website contains data form each state detailing cases and deaths from Covid-19. Each state is broken down to see cases and deaths over time. Additional graphs are provided to see a comparison of cases/deaths per 10,000 and what percentage of cases and deaths each state makes up the US. The data presented here is for public use. The accuracy of this data is not guaranteed.</p>
                 <br />
